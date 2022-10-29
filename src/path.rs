b870dfe51fa1fb731cf9e9839684bb463a181399
Patrick Willner <patrickwillner@gmx.de>
2022-10-29 23:17:29 +0200

    check for subpath in tree

--- src/path.rs
+++ src/path.rs
@@ -1,52 +1,58 @@
-use std::{
-    cell::RefCell,
-    collections::{HashMap, LinkedList},
-    hash::Hash,
-    rc::Rc,
-};
+use std::{collections::HashMap, fmt::Debug, path::Path};
 
-pub struct PathTree<K, T>
-where
-    K: Hash + Eq + PartialEq,
-{
-    children: HashMap<K, PathTree<K, T>>,
+#[derive(Debug)]
+pub struct PathTree<'a, T> {
+    children: HashMap<&'a Path, PathTree<'a, T>>,
     val: Option<T>,
 }
 
-impl<K, T> PathTree<K, T>
+impl<'a, T> PathTree<'a, T>
 where
-    K: Hash + Eq + PartialEq,
+    T: Debug,
 {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Self {
             children: HashMap::new(),
             val: None,
         }
     }
 
-    fn with_capacity(cap: usize) -> Self {
+    pub fn with_capacity(cap: usize) -> Self {
         Self {
             children: HashMap::with_capacity(cap),
             val: None,
         }
     }
 
-    fn insert(&mut self, mut keys: LinkedList<K>, val: T) {
-        let first = match keys.pop_front() {
+    pub fn insert(&mut self, path: &'a Path, val: T) {
+        let mut path_iter = path.iter();
+        let first = match path_iter.next() {
             Some(first) => first,
-            None => return,
+            None => {
+                // if the path is empty, then this node is a leaf
+                self.val = Some(val);
+                self.children.clear();
+                return;
+            }
         };
 
+        // don't add children if this node is included
+        if self.val.is_some() {
+            return;
+        }
+
         self.children
-            .entry(first)
-            .or_insert(PathTree::with_capacity(keys.len()))
-            .insert(keys, val);
+            .entry(first.as_ref())
+            .or_insert(PathTree::with_capacity(0))
+            .insert(path.strip_prefix(first).unwrap(), val);
     }
 
-    fn get(&self, keys: &LinkedList<K>) -> Option<&T> {
+    fn traverse_tree(&self, path: &'a Path) -> Option<&Self> {
         let mut tree = self;
-        for key in keys {
-            if let Some(child_tree) = tree.children.get(key) {
+        for subpath in path.iter() {
+            let subpath = tree.children.get(&subpath.as_ref());
+            println!("traverse_tree: {:?}", subpath);
+            if let Some(child_tree) = subpath {
                 tree = child_tree;
                 continue;
             }
@@ -54,6 +60,56 @@ where
             return None;
         }
 
-        tree.val.as_ref()
+        Some(tree)
+    }
+
+    pub fn get(&self, path: &'a Path) -> Option<&T> {
+        self.traverse_tree(path)?.val.as_ref()
+    }
+
+    pub fn contains_subpath(&self, subpath: &'a Path) -> bool {
+        self.traverse_tree(subpath).is_some()
+    }
+}
+
+// scenario 1:
+// PathTree contains: /tmp/a/b
+// add: /tmp/a
+// action: remove /tmp/a/**
+//
+// scenario 2:
+// PathTree contains: /tmp/a
+// add: /tmp/a/b
+// action: do nothing
+
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+
+    use super::PathTree;
+
+    #[test]
+    fn insert_and_get() {
+        let mut path_tree = PathTree::new();
+        path_tree.insert("/tmp/a/b".as_ref(), ());
+
+        assert_eq!(path_tree.get("/tmp/a/b".as_ref()), Some(&()));
+        assert_eq!(path_tree.get("/tmp/a".as_ref()), None);
+        assert_eq!(path_tree.get("/tmp/a/b/c".as_ref()), None);
+        assert_eq!(path_tree.get("/tmp/a/b.a".as_ref()), None);
+        assert_eq!(path_tree.get("tmp/a/b".as_ref()), None);
+    }
+
+    #[test]
+    fn contains_subpath() {
+        let mut path_tree = PathTree::new();
+        path_tree.insert("/tmp/a/b".as_ref(), ());
+
+        assert!(path_tree.contains_subpath("/".as_ref()));
+        assert!(path_tree.contains_subpath("/tmp".as_ref()));
+        assert!(path_tree.contains_subpath("/tmp/a".as_ref()));
+        assert!(path_tree.contains_subpath("/tmp/a/b".as_ref()));
+        assert!(!path_tree.contains_subpath("/tmp/a/c".as_ref()));
+        assert!(!path_tree.contains_subpath("tmp/a/b".as_ref()));
     }
 }
